61a59041e213acab27ef6e0c74e4d018f89a0274
enigmatic-figure <enigmatic_figure@proton.me>
2025-09-16 19:17:26 -0500

    Complete Project Review and Polish

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -8,9 +7,6 @@ const nextConfig = {
     unoptimized: true,
     domains: ['images.pexels.com', 'pexels.com']
   },
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
-  }
 };
 
 module.exports = nextConfig;
